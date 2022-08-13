474fca1d90d21110328e327059b4d38bb469a4df
tactonbishop <tactonbishop@gmail.com>
2022-08-13 20:13:18 +0100

    Set up Jest for unit tests

---
+++ src/__mocks__/vscode.ts
@@ -0,0 +1,38 @@
+export const vscode = {}
+// const window = {
+//   createStatusBarItem: jest.fn(() => ({
+//     show: jest.fn(),
+//   })),
+//   showErrorMessage: jest.fn(),
+//   showWarningMessage: jest.fn(),
+//   createTextEditorDecorationType: jest.fn(),
+// }
+
+// const workspace = {
+//   getConfiguration: jest.fn(),
+//   workspaceFolders: [],
+//   onDidSaveTextDocument: jest.fn(),
+// }
+
+// const Uri = {
+//   file: (f) => f,
+//   parse: jest.fn(),
+// }
+// const debug = {
+//   onDidTerminateDebugSession: jest.fn(),
+//   startDebugging: jest.fn(),
+// }
+
+// const commands = {
+//   executeCommand: jest.fn(),
+// }
+
+// const vscode = {
+//   window,
+//   workspace,
+//   Uri,
+//   debug,
+//   commands,
+// }
+
+// module.exports = vscode

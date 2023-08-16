2ffc0ca74605acde93654d7623efed48e4c160e7
thomasabishop <tactonbishop@gmail.com>
2023-08-16 20:48:40 +0100

    feature: add warning option to channel output

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -4,10 +4,17 @@ let outputChannel: any = vscode.window.createOutputChannel("Meridian", {
    log: true,
 })
 
-export function printChannelOutput(content: string, reveal = false, logtype?: string): void {
+export function printChannelOutput(
+   content: string,
+   reveal = false,
+   logtype?: string
+): void {
    if (logtype === "error") {
       outputChannel.show(true)
       outputChannel.error(content)
+   } else if (logtype === "warning") {
+      reveal && outputChannel.show(true)
+      outputChannel.appendLine(`[warning] ${content}`)
    } else {
       reveal && outputChannel.show(true)
       outputChannel.appendLine(content)
